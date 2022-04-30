8c0b78e8297a846df0a9ac7b5b5cdf1f3e21ad71
shivambhandari99 <bhand092@umn.edu>
2022-04-30 11:14:42 -0500

    GPU

--- code/ConvLSTM.py
+++ code/ConvLSTM.py
@@ -36,8 +36,11 @@ class ConvLSTMCell(nn.Module):
     def init_hidden(self, batch_size, image_size):
         """ initialize the first hidden state as zeros """
         height, width, dimensions = image_size
+        return (torch.zeros(batch_size, self.h_channels, height, width, dimensions),
+                torch.zeros(batch_size, self.h_channels, height, width, dimensions))
         return (torch.zeros(batch_size, self.h_channels, height, width, dimensions,device=self.device),
                 torch.zeros(batch_size, self.h_channels, height, width, dimensions,device=self.device))
+
 
 
 class ConvLSTM(nn.Module):
