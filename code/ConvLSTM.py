43138bc644bd7dc02cf9bee38d83081d7360a081
shivambhandari99 <bhand092@umn.edu>
2022-04-29 23:44:53 -0500

    GPU

--- code/ConvLSTM.py
+++ code/ConvLSTM.py
@@ -180,7 +180,7 @@ class Seq2Seq(nn.Module):
         self.seq_len = args.seq_len
         self.horizon = args.horizon
         self.in_channels = args.in_channels
-
+        self.gpu = args.gpu_id
         self.frame_encoder = DCGAN64Encoder(self.in_channels, self.h_channels).to(self.gpu)
         self.frame_decoder = DCGAN64Decoder(self.h_channels, self.out_channels).to(self.gpu)
 
