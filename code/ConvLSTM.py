80f4e6e337652a38b79eb0ef0ba189555742d24a
shivambhandari99 <bhand092@umn.edu>
2022-04-30 10:50:05 -0500

    GPU

--- code/ConvLSTM.py
+++ code/ConvLSTM.py
@@ -73,7 +73,7 @@ class ConvLSTM(nn.Module):
         image_size = (x.size(dim=2),x.size(dim=3),x.size(dim=1))
         batch_size = (x.size(dim=0))
         if states[0] is None:
-            print(self.init_hidden(batch_size, image_size)[1])
+            print(len(self.init_hidden(batch_size, image_size)[1]))
             hidden_states, cell_states = self.init_hidden(batch_size, image_size)
         else:
             hidden_states, cell_states = states
